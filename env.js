23ce30195c11089ccccd7807731788bb69f62b54
Semenov <Semenov@mail.ru>
2024-03-23 10:49:38 +0400

    вернул конфиг для старого сайта

--- env.js
+++ env.js
@@ -1,20 +1,15 @@
 export const ENV = {
   // сайт куда зайди
-  PATH: "https://wayout.zone/ru/",
+  PATH: "http://lohmatik.ru/Lohmatik/str1_2_5.php",
   // какой dom элементы нужно дождаться
-  INIT_DOM_SELECTOR: ["#mazeCanvas"],
-  CLICK_DOM_SELECTOR: ["#startMazeBtn"],
+  INIT_DOM_SELECTOR: ["#labdiv"],
   // из чего получить верстку лабиринта
+  RETURN_HTML_DOM: "#labdiv",
   // случайное время ожидание перед кликом от MIN до MAX
   DELAY_PRE: { MIN: 100, MAX: 200 },
   // случайное время ожидание после клика от MIN до MAX
   DElAY_POST: { MIN: 100, MAX: 200 },
   HISTORY: false,
   CONSOLE: true,
-  PASRING_ING: true,
-  COOKIES: {
-    cookie_settings: "%7B%22cookie_show%22%3A%22%22%2C%22dark_mode%22%3A%22no%22%7D",
-    csrftoken: "9hqwfkZuXcP9jpdpwSSPcncEOzfwv7xpI2fFLLjM5ztzU4VemVvVAFWb6AgJjPaa",
-    sessionid: "2u0zit5nptm9fkxwiqwc7dqvuuwqxcia"
-  }
+  PASRING_ING: false
 };
